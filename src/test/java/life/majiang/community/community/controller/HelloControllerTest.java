fc28496ffc7304774785e1ae23cfb0072475d26c
small_artisan <1912436696@qq.com>
2019-08-29 16:01:21 +0800

    init report

---
+++ src/test/java/life/majiang/community/community/controller/HelloControllerTest.java
@@ -0,0 +1,24 @@
+package life.majiang.community.community.controller;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class HelloControllerTest {
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addClass(HelloController.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+    @org.jboss.arquillian.container.test.api.Deployment
+    public static org.jboss.shrinkwrap.api.Archive createDeployment1() {
+        return org.jboss.shrinkwrap.api.ShrinkWrap.create(org.jboss.shrinkwrap.api.spec.JavaArchive.class)
+                .addAsManifestResource(org.jboss.shrinkwrap.api.asset.EmptyAsset.INSTANCE, "beans.xml");
+    }
+}
